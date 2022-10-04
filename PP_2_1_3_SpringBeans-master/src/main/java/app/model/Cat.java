bac9da065cfe0023f3a6d86c19ec4b5303735f76
piotrek123411 <83031705+piotrek123411@users.noreply.github.com>
2022-10-04 20:07:44 +0300

    i

--- PP_2_1_3_SpringBeans-master/src/main/java/app/model/Cat.java
+++ PP_2_1_3_SpringBeans-master/src/main/java/app/model/Cat.java
@@ -1,11 +1,13 @@
 package app.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Cat extends Animal {
 
     private String says;
+    @Autowired
     public Cat(String says) {
         this.says = says;
     }
--- PP_2_1_3_SpringBeans-master/src/main/java/app/model/Dog.java
+++ PP_2_1_3_SpringBeans-master/src/main/java/app/model/Dog.java
@@ -1,10 +1,12 @@
 package app.model;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Dog extends Animal {
 
     private String says;
+    @Autowired
     public Dog(String says) {
         this.says = says;
     }
